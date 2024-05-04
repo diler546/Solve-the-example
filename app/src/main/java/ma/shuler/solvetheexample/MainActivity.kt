60b0025a1935d7b3632038df04bf0a3dc1208414
makar <makar921@mail.ru>
2024-05-04 14:03:48 +0300

    add UI

---
+++ app/src/main/java/ma/shuler/solvetheexample/MainActivity.kt
@@ -0,0 +1,11 @@
+package ma.shuler.solvetheexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
